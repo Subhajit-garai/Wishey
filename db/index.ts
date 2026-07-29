5beb66f476c02694e1acbce08ac8d987c07bf93f
Subhajit Garai <subhajitgarai988@gmail.com>
2026-07-29 16:03:43 +0530

    fix db error

--- db/index.ts
+++ db/index.ts
@@ -2,13 +2,21 @@ import { drizzle } from "drizzle-orm/node-postgres";
 import { Pool } from "pg";
 import * as schema from "./schema";
 
-const connectionString = process.env.DATABASE_URL || "postgres://postgres:postgres@localhost:5432/wishey_db";
+const connectionString = process.env.DATABASE_URL!;
 
 const pool = new Pool({
   connectionString,
+  ssl: process.env.DB_CA_CERT
+    ? {
+        rejectUnauthorized: true,
+        ca: process.env.DB_CA_CERT,
+      }
+    : {
+        rejectUnauthorized: false,
+      },
   max: 10,
   idleTimeoutMillis: 30000,
-  connectionTimeoutMillis: 2000,
+  connectionTimeoutMillis: 5000,
 });
 
 export const db = drizzle(pool, { schema });
